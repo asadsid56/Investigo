b9c92863966f433f3e20bcd1239a49a609605d80
asadsid56 <asadsid56@gmail.com>
2022-03-11 15:01:11 +0100

    new changes

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ app.post("/register", async (req,res) => {
     
     const create_UUID = () => {
         let dt = new Date().getTime();
-        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let uuid = 'xxxxxxxx'.replace(/[xy]/g, function(c) {
             let r = (dt + Math.random()*16)%16 | 0;
             dt = Math.floor(dt/16);
             return (c=='x' ? r :(r&0x3|0x8)).toString(16);
